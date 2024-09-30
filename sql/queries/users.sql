e04247d3280ef44cf4a73f6081603350bceb48a3
JehadAbdulwafi <jehad.a6k@gmail.com>
2024-09-30 08:43:02 +0200

    add query CheckUserExists

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -7,6 +7,9 @@ RETURNING *;
 -- name: GetUserByID :one
 SELECT * FROM users WHERE id = $1;
 
+-- name: CheckUserExists :one
+SELECT EXISTS (SELECT 1 FROM users WHERE email = $1);
+
 -- name: GetUserByEmail :one
 SELECT * FROM users WHERE email = $1;
 
