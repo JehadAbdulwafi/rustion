10ebb16b2788f0df080391a2bc6d08614489426d
JehadAbdulwafi <jehad.a6k@gmail.com>
2024-10-04 17:14:10 +0200

    feat: Initialize Expo app and set up Tamagui
    - Initialized a new Expo app with the name "rustion".
    - Added necessary dependencies including:
      - @expo/metro-runtime
      - @expo/vector-icons
      - @react-native-async-storage/async-storage
      - @react-navigation/native
      - @shopify/flash-list
      - @tamagui/config
      - @tamagui/lucide-icons
      - @tamagui/metro-plugin
      - @tamagui/toast
      - axios
      - expo and related packages
      - react and react-native
      - zod and zustand for state management
    - Configured Tamagui for styling and UI components.

---
+++ moblie/app/_layout.tsx
@@ -0,0 +1,35 @@
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+import { Provider } from '@/components/Provider';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <Provider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </Provider>
+  );
+}
