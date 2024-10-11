ef4c542593c5a3340e5090f29a367cfe4116a16d
JehadAbdulwafi <jehad.a6k@gmail.com>
2024-10-11 22:46:34 +0200

    add new tab page

--- moblie/app/(tabs)/_layout.tsx
+++ moblie/app/(tabs)/_layout.tsx
@@ -4,21 +4,27 @@ export default function TabLayout() {
   return (
     <Stack>
       <Stack.Screen
-        name="player"
-        options={{
-          title: "Player",
-        }}
-      />
-      <Stack.Screen
         name="index"
         options={{
           title: "Home",
         }}
       />
       <Stack.Screen
+        name="player"
+        options={{
+          title: "Player",
+        }}
+      />
+      <Stack.Screen
         name="explore"
         options={{
           title: "Explore",
+        }}
+      />
+      <Stack.Screen
+        name="account"
+        options={{
+          title: "Account",
         }}
       />
     </Stack>
---
+++ moblie/app/(tabs)/account.tsx
@@ -0,0 +1,5 @@
+import { View } from "@/components/ui/View";
+
+export default function Account() {
+  return <View></View>;
+}
