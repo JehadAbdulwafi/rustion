067dd48cc4489b8ef4af45b064e189b7096f3f3e
JehadAbdulwafi <jehad.a6k@gmail.com>
2024-10-17 23:17:23 +0200

    Update HomeScreen to include Categories and FeaturedRows in a ScrollView

--- moblie/app/(tabs)/index.tsx
+++ moblie/app/(tabs)/index.tsx
@@ -1,10 +1,24 @@
 import Carousel from "@/components/carousel";
+import Categories from "@/components/categories";
+import FeaturedRows from "@/components/featuredRows";
 import { View } from "@/components/ui/View";
+import { navigationHeight } from "@/constants";
+import { FEATURED_ROWS } from "@/constants/data";
+import { ScrollView } from "tamagui";
 
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1 }}>
-      <Carousel />
+      <ScrollView
+        f={1}
+        contentContainerStyle={{
+          paddingBottom: navigationHeight + 20,
+        }}
+      >
+        <Carousel />
+        <Categories />
+        <FeaturedRows data={FEATURED_ROWS} />
+      </ScrollView>
     </View>
   );
 }
