1ba699eb50709c050e0a35b0c695ac462ea3830f
JehadAbdulwafi <jehad.a6k@gmail.com>
2024-10-29 08:20:35 +0200

    feat: impl home screen

--- moblie/app/(tabs)/index.tsx
+++ moblie/app/(tabs)/index.tsx
@@ -1,14 +1,45 @@
+import { getCategories } from "@/api/CategoryApi";
+import { getFeaturedSectionsWithArticles } from "@/api/FeaturedSectionApi";
 import Carousel from "@/components/carousel";
 import Categories from "@/components/categories";
 import FeaturedRows from "@/components/featuredRows";
 import { View } from "@/components/ui/View";
 import { navigationHeight } from "@/constants";
-import { FEATURED_ROWS } from "@/constants/data";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, StatusBar } from "react-native";
 import { ScrollView } from "tamagui";
 
 export default function HomeScreen() {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [featuredSections, setFeaturedSections] = useState<FeaturedSectionWithArticles[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      try {
+        const [_categories, _featuredSections] = await Promise.all([getCategories(), getFeaturedSectionsWithArticles()]);
+        setCategories(_categories);
+        setFeaturedSections(_featuredSections);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    )();
+  }, []);
+
+  if (loading) {
+    return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ActivityIndicator size="large" color="white" />
+    </View>;
+  }
+
+
   return (
     <View style={{ flex: 1 }}>
+      <StatusBar backgroundColor="transparent" barStyle="light-content" />
       <ScrollView
         f={1}
         contentContainerStyle={{
@@ -16,8 +47,8 @@ export default function HomeScreen() {
         }}
       >
         <Carousel />
-        <Categories />
-        <FeaturedRows data={FEATURED_ROWS} />
+        <Categories data={categories} />
+        <FeaturedRows data={featuredSections} />
       </ScrollView>
     </View>
   );
