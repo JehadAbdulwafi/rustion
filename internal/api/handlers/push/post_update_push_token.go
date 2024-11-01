0d8496748c42ba203f1d28febf89a711012c1717
JehadAbdulwafi <jehad.a6k@gmail.com>
2024-11-01 18:17:14 +0200

    add update and test push routes

---
+++ internal/api/handlers/push/post_update_push_token.go
@@ -0,0 +1,79 @@
+package push
+
+import (
+	"database/sql"
+	"errors"
+	"net/http"
+
+	"github.com/JehadAbdulwafi/rustion/internal/api"
+	"github.com/JehadAbdulwafi/rustion/internal/api/httperrors"
+	"github.com/JehadAbdulwafi/rustion/internal/database"
+	"github.com/JehadAbdulwafi/rustion/internal/types"
+	"github.com/JehadAbdulwafi/rustion/internal/util"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/lib/pq"
+)
+
+func PostUpdatePushTokenRoute(s *api.Server) *echo.Route {
+	return s.Router.APIV1Push.PUT("/token", postUpdatePushTokenHandler(s))
+}
+
+func postUpdatePushTokenHandler(s *api.Server) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		ctx := c.Request().Context()
+		log := util.LogFromContext(ctx)
+
+		var body types.PostUpdatePushTokenPayload
+		if err := util.BindAndValidateBody(c, &body); err != nil {
+			return err
+		}
+
+		userID, err := uuid.Parse(body.UserID.String())
+		if err != nil {
+			log.Debug().Str("user_id", body.UserID.String()).Err(err).Msg("Failed to parse user id.")
+			return err
+		}
+
+		// insert new token
+		if _, err := s.Queries.CreatePushToken(ctx, database.CreatePushTokenParams{
+			Provider: database.ProviderTypeFcm,
+			Token:    *body.NewToken,
+			UserID:   userID,
+		}); err != nil {
+			log.Debug().Str("user_id", userID.String()).Err(err).Msg("Failed to insert push token.")
+			 
+			// check for unique_violation on token column, 23505 == unique_violation
+			var pqErr *pq.Error
+			if errors.As(err, &pqErr) {
+				if pqErr.Code == "23505" && pqErr.Constraint == "push_tokens_token_key" {
+					return httperrors.ErrConflictPushToken
+				}
+			}
+
+			return err
+		}
+
+		// delete old token if present in request
+		if body.OldToken != nil {
+			oldToken, err := s.Queries.GetPushToken(ctx, *body.OldToken)
+			if err != nil {
+				log.Debug().Str("user_id", userID.String()).Err(err).Msg("Old token to delete not found or not assigned to body.")
+				if errors.Is(err, sql.ErrNoRows) {
+					return httperrors.ErrNotFoundOldPushToken
+				}
+
+				return err
+			}
+
+			if err := s.Queries.DeletePushToken(ctx, oldToken.Token); err != nil {
+				log.Debug().Str("user_id", userID.String()).Err(err).Msg("Failed to delete old push token.")
+				return err
+			}
+		}
+
+		log.Debug().Str("user_id", userID.String()).Msg("Successfully updated push token.")
+
+		return c.String(http.StatusOK, "Success")
+	}
+}
--- internal/types/spec_handlers.go
+++ internal/types/spec_handlers.go
@@ -50,6 +50,7 @@ func (o *SwaggerSpec) initHandlerCache() {
 	o.Handlers["GET"]["/api/v1/featured-sections/{id}/articles"] = true
 	o.Handlers["GET"]["/api/v1/featured-sections/articles"] = true
 	o.Handlers["GET"]["/api/v1/healthy"] = true
+	o.Handlers["GET"]["/api/v1/push/test"] = true
 	o.Handlers["GET"]["/swagger.yml"] = true
 	o.Handlers["GET"]["/api/v1/auth/userinfo"] = true
 	o.Handlers["GET"]["/api/v1/version"] = true
@@ -63,6 +64,7 @@ func (o *SwaggerSpec) initHandlerCache() {
 	o.Handlers["POST"]["/api/v1/auth/register"] = true
 	o.Handlers["POST"]["/api/v1/streams/stop"] = true
 	o.Handlers["POST"]["/api/v1/streams/unpublish"] = true
+	o.Handlers["PUT"]["/api/v1/push/token"] = true
 	o.Handlers["PUT"]["/api/v1/articles/{id}"] = true
 	o.Handlers["PUT"]["/api/v1/categories/{id}"] = true
 	o.Handlers["PUT"]["/api/v1/featured-sections/{id}"] = true
