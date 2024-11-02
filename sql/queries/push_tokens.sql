13c384ff1d101ae0460e71f4259766feede5d764
JehadAbdulwafi <jehad.a6k@gmail.com>
2024-11-02 16:19:05 +0200

    feat(sql-push): repalce userId with fingerprint

--- sql/queries/push_tokens.sql
+++ sql/queries/push_tokens.sql
@@ -1,22 +1,19 @@
 -- name: CreatePushToken :one
-INSERT INTO push_tokens (token, provider, user_id)
+INSERT INTO push_tokens (token, fingerprint, provider)
 VALUES ($1, $2, $3)
 RETURNING *;
 
 -- name: GetPushToken :one
 SELECT * FROM push_tokens WHERE token = $1;
 
--- name: GetPushTokensByUserID :many
-SELECT * FROM push_tokens WHERE user_id = $1;
+-- name: GetPushTokensByID :one
+SELECT * FROM push_tokens WHERE id = $1;
 
--- name: GetPushTokensByUserIDAndProvider :many
-SELECT * FROM push_tokens WHERE user_id = $1 AND provider = $2;
+-- name: GetPushTokensByFingerprint :many
+SELECT * FROM push_tokens WHERE fingerprint = $1;
 
 -- name: DeletePushToken :exec
 DELETE FROM push_tokens WHERE token = $1;
 
--- name: DeletePushTokensByUserID :exec
-DELETE FROM push_tokens WHERE user_id = $1;
-
--- name: DeletePushTokensByUserIDAndProvider :exec
-DELETE FROM push_tokens WHERE user_id = $1 AND provider = $2;
+-- name: DeletePushTokensByFingerprint :exec
+DELETE FROM push_tokens WHERE fingerprint = $1;
--- sql/schema/0005_create_push_tokens_table.sql
+++ sql/schema/0005_create_push_tokens_table.sql
@@ -8,13 +8,13 @@ CREATE TABLE push_tokens (
     id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
     token TEXT UNIQUE NOT NULL,
     provider provider_type NOT NULL,
-    user_id UUID NOT NULL REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    fingerprint TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
 
-CREATE INDEX idx_push_tokens_fk_user_id ON push_tokens USING btree (user_id);
 CREATE INDEX idx_push_tokens_token ON push_tokens USING btree (token);
+CREATE INDEX idx_push_tokens_fingerprint ON push_tokens USING btree (fingerprint);
 
 -- +goose Down
 DROP TABLE IF EXISTS push_tokens;
