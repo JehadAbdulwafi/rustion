aad8e3dc131c550173b4e68b4f9c45d03ded3089
JehadAbdulwafi <jehad.a6k@gmail.com>
2024-11-17 22:09:46 +0200

    chore: change local ip address

--- moblie/api/config.ts
+++ moblie/api/config.ts
@@ -2,7 +2,7 @@ const scheme = "http";
 // const scheme = "https";
 
 // const host = "localhost:9973";
-const host = "192.168.1.6:9973";
+const host = "192.168.1.5:9973";
 
 const origin = `${scheme}://${host}`;
 
