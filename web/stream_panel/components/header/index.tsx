0f439063c7bd7180f058db1966d8a46e168c627e
JehadAbdulwafi <jehad.a6k@gmail.com>
2024-12-01 12:14:15 +0200

    feat: Implement responsive header navigation

---
+++ web/stream_panel/components/header/index.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+import { siteConfig } from "@/config/site";
+import { Icons } from "@/components/icons";
+import { MainNav } from "@/components/header/main-nav";
+import { MobileNav } from "@/components/header/mobile-nav";
+import { ModeSwitcher } from "@/components/header/mode-switcher";
+import { Button } from "@/components/ui/button";
+
+export function Header() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:border-border">
+      <div className="flex h-14 items-center px-4">
+        <MainNav />
+        <MobileNav />
+        <div className="flex flex-1 items-center justify-between gap-2 md:justify-end">
+          <nav className="flex items-center gap-0.5">
+            <Button variant="ghost" size="icon" className="h-8 w-8 px-0">
+              <Link
+                href={siteConfig.links.github}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Icons.gitHub className="h-4 w-4" />
+                <span className="sr-only">GitHub</span>
+              </Link>
+            </Button>
+            <ModeSwitcher />
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
