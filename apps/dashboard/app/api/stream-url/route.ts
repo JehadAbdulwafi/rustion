bc7b1c6024feb2903a041231a0e0755817006942
JehadAbdulwafi <jehad.a6k@gmail.com>
2025-01-12 17:00:49 +0200

    fix srt url

--- apps/dashboard/app/api/stream-url/route.ts
+++ apps/dashboard/app/api/stream-url/route.ts
@@ -3,14 +3,14 @@ import { NextResponse } from 'next/server';
 export async function POST(request: Request) {
   const data = await request.json();
   const { stream } = data;
-  
+
   const API_SECRET = process.env.API_SECRET;
-  
-  const srtUrl = `srt://${stream.host || 'localhost'}:10800?streamid=#!::r=${stream.app}/${stream.name}?secret=${API_SECRET}&password=${stream.password},m=publish`;
+
+  const srtUrl = `srt://${stream.host || 'localhost'}:10080?streamid=#!::r=live/${stream.endpoint},secret=${API_SECRET},password=${stream.password},m=publish`;
   const rtmpUrl = `rtmp://${stream.host || 'localhost'}/${stream.app || ''}`;
-  const rtmpKey = `/${stream.name}?secret=${API_SECRET}&password=${stream.password}`;
-  
-  return NextResponse.json({ 
+  const rtmpKey = `/${stream.endpoint}?secret=${API_SECRET}&password=${stream.password}`;
+
+  return NextResponse.json({
     srtUrl,
     rtmpUrl,
     rtmpKey
