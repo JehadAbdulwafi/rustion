c58a77e480ca80ae1f7ec36176592a1e2acd2ff4
JehadAbdulwafi <jehad.a6k@gmail.com>
2025-01-12 22:29:33 +0200

    fix: move password validation to publish action only

--- internal/api/handlers/stream/post_events_stream.go
+++ internal/api/handlers/stream/post_events_stream.go
@@ -22,38 +22,29 @@ func postStreamEventsHandler(s *api.Server) echo.HandlerFunc {
 			return err
 		}
 
-		log.Debug().Interface("body", body).Msg("request body")
-
-		if body.Param == nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "param is required")
-		}
-
-		values, err := url.ParseQuery(*body.Param)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "invalid param format")
-		}
-		password := values.Get("password")
-		
-		log.Debug().Str("password", password).Msg("request password")
-
-		if password == "" {
-		log.Debug().Str("password", password).Msg("password is empty")
-			return echo.NewHTTPError(http.StatusBadRequest, "password is required")
-		}
-
 		stream, err := s.Queries.GetStreamByEndpoint(c.Request().Context(), *body.Stream)
 		if err != nil {
 			log.Error().Err(err).Msg("Failed to get stream")
 			return echo.NewHTTPError(http.StatusNotFound, "Stream not found")
 		}
 
-		if password != stream.Password {
-			log.Error().Msg("Invalid password")
-			return echo.NewHTTPError(http.StatusUnauthorized, "Unauthorized")
-		}
-
 		switch *body.Action {
 		case util.StreamActionPublish:
+			values, err := url.ParseQuery(body.Param)
+			if err != nil {
+				return echo.NewHTTPError(http.StatusBadRequest, "invalid param format")
+			}
+
+			password := values.Get("password")
+			if password == "" {
+				return echo.NewHTTPError(http.StatusBadRequest, "password is required")
+			}
+
+			if password != stream.Password {
+				log.Error().Msg("Invalid password")
+				return echo.NewHTTPError(http.StatusUnauthorized, "Unauthorized")
+			}
+
 			err = s.Queries.PublishStream(c.Request().Context(), stream.ID)
 			if err != nil {
 				log.Error().Err(err).Msg("Failed to publish stream")
@@ -73,8 +64,6 @@ func postStreamEventsHandler(s *api.Server) echo.HandlerFunc {
 		res := &types.StreamEventResponse{
 			Code: &code,
 		}
-
-		log.Debug().Interface("res", res).Msg("response")
 
 		return util.ValidateAndReturn(c, http.StatusOK, res)
 	}
