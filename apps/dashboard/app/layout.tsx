f8dc84a30d9b2ecc22ba9abe2728a38dab3616a9
JehadAbdulwafi <jehad.a6k@gmail.com>
2025-01-15 16:29:26 +0200

    add tailwind indicator

--- apps/dashboard/app/layout.tsx
+++ apps/dashboard/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "../styles/globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { TailwindIndicator } from "@/components/tailwind-indicator";
 
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
@@ -40,6 +41,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <TailwindIndicator />
         </ThemeProvider>
       </body>
     </html>
