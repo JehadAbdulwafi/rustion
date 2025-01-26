5cc808efa0bf61e125c49a7cedf4596d6aa54f35
JehadAbdulwafi <jehad.a6k@gmail.com>
2025-01-26 15:52:12 +0200

    fix cancel subscription

--- internal/api/handlers/subscription/cancel_active_subscription.go
+++ internal/api/handlers/subscription/cancel_active_subscription.go
@@ -5,21 +5,32 @@ import (
 
 	"github.com/JehadAbdulwafi/rustion/internal/api"
 	"github.com/JehadAbdulwafi/rustion/internal/api/auth"
+	"github.com/JehadAbdulwafi/rustion/internal/database"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
 func DeleteCancelSubscriptionRoute(s *api.Server) *echo.Route {
-	return s.Router.APIV1Subscription.DELETE("/cancel", deleteCancelSubscriptionHandler(s))
+	return s.Router.APIV1Subscription.DELETE("/:id/cancel", deleteCancelSubscriptionHandler(s))
 }
 
 func deleteCancelSubscriptionHandler(s *api.Server) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		ctx := c.Request().Context()
-		user := auth.UserFromContext(ctx)
+		_ = auth.UserFromContext(ctx)
 
-		subscription, err := s.Queries.GetUserActiveSubscription(ctx, user.ID)
+		id := c.Param("id")
+		if id == "" {
+			return echo.NewHTTPError(http.StatusBadRequest, "Invalid subscription ID")
+		}
+
+		subscription, err := s.Queries.GetSubscription(ctx, uuid.MustParse(id))
 		if err != nil {
-			return echo.NewHTTPError(http.StatusNotFound, "No active subscription found")
+			return echo.NewHTTPError(http.StatusNotFound, "Subscription not found")
+		}
+
+		if subscription.Status != database.SubscriptionStatusEnumActive {
+			return echo.NewHTTPError(http.StatusNotFound, "your subscription is not active")
 		}
 
 		err = s.Queries.CancelSubscription(ctx, subscription.ID)
