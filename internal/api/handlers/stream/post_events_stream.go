a8fe6c9fe7ad8463b930cf48c713b627b24189a2
JehadAbdulwafi <jehad.a6k@gmail.com>
2025-01-28 07:39:26 +0200

    comment out some stuff

--- internal/api/handlers/stream/post_events_stream.go
+++ internal/api/handlers/stream/post_events_stream.go
@@ -19,85 +19,85 @@ func PostStreamEventsRoute(s *api.Server) *echo.Route {
 
 func postStreamEventsHandler(s *api.Server) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		ctx := c.Request().Context()
-		var body types.StreamEvent
-		if err := util.BindAndValidateBody(c, &body); err != nil {
-			return err
-		}
-
-		stream, err := s.Queries.GetStreamByEndpoint(ctx, *body.Stream)
-		if err != nil {
-			log.Error().Err(err).Msg("Failed to get stream")
-			return echo.NewHTTPError(http.StatusNotFound, "Stream not found")
-		}
-
-		// activeSub, err := s.Queries.GetUserActiveSubscription(ctx, stream.UserID)
-		// if err != nil {
-		// 	log.Error().Err(err).Msg("Failed to get user active subscription")
-		// 	return echo.NewHTTPError(http.StatusInternalServerError, "Failed to get user active subscription")
+		// ctx := c.Request().Context()
+		// var body types.StreamEvent
+		// if err := util.BindAndValidateBody(c, &body); err != nil {
+		// 	return err
 		// }
-
-		switch *body.Action {
-		case util.StreamActionPublish:
-			values, err := url.ParseQuery(body.Param)
-			if err != nil {
-				return echo.NewHTTPError(http.StatusBadRequest, "invalid param format")
-			}
-
-			password := values.Get("password")
-			if password == "" {
-				return echo.NewHTTPError(http.StatusBadRequest, "password is required")
-			}
-
-			if password != stream.Password {
-				log.Error().Msg("Invalid password")
-				return echo.NewHTTPError(http.StatusUnauthorized, "Unauthorized")
-			}
-
-			err = s.Queries.PublishStream(ctx, stream.ID)
-			if err != nil {
-				log.Error().Err(err).Msg("Failed to publish stream")
-				return echo.NewHTTPError(http.StatusInternalServerError, "Failed to publish stream")
-			}
-			// Broadcast status update to all connected clients
-			BroadcastStreamStatus(s, stream.ID)
-
-		case util.StreamActionUnpublish:
-			// // Get the stream's last published time to calculate duration
-			// streamInfo, err := s.Queries.GetStreamById(ctx, stream.ID)
-			// if err != nil {
-			// 	log.Error().Err(err).Msg("Failed to get stream info")
-			// 	return echo.NewHTTPError(http.StatusInternalServerError, "Failed to get stream info")
-			// }
-			//
-			// if streamInfo.LastPublishedAt.Valid {
-			// 	// Calculate streaming duration in minutes
-			// 	duration := time.Since(streamInfo.LastPublishedAt.Time)
-			// 	streamingMinutes := int32(duration.Minutes())
-			//
-			// 	// Update daily streaming usage
-			// 	err = s.Queries.UpdateDailyStreamingUsage(ctx, database.UpdateDailyStreamingUsageParams{
-			// 		UserID:               streamInfo.UserID,
-			// 		SubscriptionID:       activeSub.ID,
-			// 		StreamingMinutesUsed: streamingMinutes,
-			// 		UsageDate:            time.Now().UTC().Truncate(24 * time.Hour),
-			// 	})
-			// 	if err != nil {
-			// 		log.Error().Err(err).Msg("Failed to update streaming usage")
-			// 	}
-			// }
-			//
-			err = s.Queries.UnpublishStream(ctx, stream.ID)
-			if err != nil {
-				log.Error().Err(err).Msg("Failed to unpublish stream")
-				return echo.NewHTTPError(http.StatusInternalServerError, "Failed to unpublish stream")
-			}
-			// Broadcast status update to all connected clients
-			BroadcastStreamStatus(s, stream.ID)
-
-		default:
-			log.Info().Str("action", *body.Action).Msg("invalid action")
-		}
+		//
+		// stream, err := s.Queries.GetStreamByEndpoint(ctx, *body.Stream)
+		// if err != nil {
+		// 	log.Error().Err(err).Msg("Failed to get stream")
+		// 	return echo.NewHTTPError(http.StatusNotFound, "Stream not found")
+		// }
+		//
+		// // activeSub, err := s.Queries.GetUserActiveSubscription(ctx, stream.UserID)
+		// // if err != nil {
+		// // 	log.Error().Err(err).Msg("Failed to get user active subscription")
+		// // 	return echo.NewHTTPError(http.StatusInternalServerError, "Failed to get user active subscription")
+		// // }
+		//
+		// switch *body.Action {
+		// case util.StreamActionPublish:
+		// 	values, err := url.ParseQuery(body.Param)
+		// 	if err != nil {
+		// 		return echo.NewHTTPError(http.StatusBadRequest, "invalid param format")
+		// 	}
+		//
+		// 	password := values.Get("password")
+		// 	if password == "" {
+		// 		return echo.NewHTTPError(http.StatusBadRequest, "password is required")
+		// 	}
+		//
+		// 	if password != stream.Password {
+		// 		log.Error().Msg("Invalid password")
+		// 		return echo.NewHTTPError(http.StatusUnauthorized, "Unauthorized")
+		// 	}
+		//
+		// 	err = s.Queries.PublishStream(ctx, stream.ID)
+		// 	if err != nil {
+		// 		log.Error().Err(err).Msg("Failed to publish stream")
+		// 		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to publish stream")
+		// 	}
+		// 	// Broadcast status update to all connected clients
+		// 	BroadcastStreamStatus(s, stream.ID)
+		//
+		// case util.StreamActionUnpublish:
+		// 	// // Get the stream's last published time to calculate duration
+		// 	// streamInfo, err := s.Queries.GetStreamById(ctx, stream.ID)
+		// 	// if err != nil {
+		// 	// 	log.Error().Err(err).Msg("Failed to get stream info")
+		// 	// 	return echo.NewHTTPError(http.StatusInternalServerError, "Failed to get stream info")
+		// 	// }
+		// 	//
+		// 	// if streamInfo.LastPublishedAt.Valid {
+		// 	// 	// Calculate streaming duration in minutes
+		// 	// 	duration := time.Since(streamInfo.LastPublishedAt.Time)
+		// 	// 	streamingMinutes := int32(duration.Minutes())
+		// 	//
+		// 	// 	// Update daily streaming usage
+		// 	// 	err = s.Queries.UpdateDailyStreamingUsage(ctx, database.UpdateDailyStreamingUsageParams{
+		// 	// 		UserID:               streamInfo.UserID,
+		// 	// 		SubscriptionID:       activeSub.ID,
+		// 	// 		StreamingMinutesUsed: streamingMinutes,
+		// 	// 		UsageDate:            time.Now().UTC().Truncate(24 * time.Hour),
+		// 	// 	})
+		// 	// 	if err != nil {
+		// 	// 		log.Error().Err(err).Msg("Failed to update streaming usage")
+		// 	// 	}
+		// 	// }
+		// 	//
+		// 	err = s.Queries.UnpublishStream(ctx, stream.ID)
+		// 	if err != nil {
+		// 		log.Error().Err(err).Msg("Failed to unpublish stream")
+		// 		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to unpublish stream")
+		// 	}
+		// 	// Broadcast status update to all connected clients
+		// 	BroadcastStreamStatus(s, stream.ID)
+		//
+		// default:
+		// 	log.Info().Str("action", *body.Action).Msg("invalid action")
+		// }
 
 		code := int64(0)
 		res := &types.StreamEventResponse{
