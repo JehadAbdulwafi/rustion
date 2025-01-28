78847b395d9ad7b1b5361c6b4759264a481c28e1
JehadAbdulwafi <jehad.a6k@gmail.com>
2025-01-28 07:26:10 +0200

    comment out some stuff, something broken

--- internal/api/handlers/stream/post_events_stream.go
+++ internal/api/handlers/stream/post_events_stream.go
@@ -3,10 +3,10 @@ package stream
 import (
 	"net/http"
 	"net/url"
-	"time"
+	// "time"
 
 	"github.com/JehadAbdulwafi/rustion/internal/api"
-	"github.com/JehadAbdulwafi/rustion/internal/database"
+	// "github.com/JehadAbdulwafi/rustion/internal/database"
 	"github.com/JehadAbdulwafi/rustion/internal/types"
 	"github.com/JehadAbdulwafi/rustion/internal/util"
 	"github.com/labstack/echo/v4"
@@ -31,11 +31,11 @@ func postStreamEventsHandler(s *api.Server) echo.HandlerFunc {
 			return echo.NewHTTPError(http.StatusNotFound, "Stream not found")
 		}
 
-		activeSub, err := s.Queries.GetUserActiveSubscription(ctx, stream.UserID)
-		if err != nil {
-			log.Error().Err(err).Msg("Failed to get user active subscription")
-			return echo.NewHTTPError(http.StatusInternalServerError, "Failed to get user active subscription")
-		}
+		// activeSub, err := s.Queries.GetUserActiveSubscription(ctx, stream.UserID)
+		// if err != nil {
+		// 	log.Error().Err(err).Msg("Failed to get user active subscription")
+		// 	return echo.NewHTTPError(http.StatusInternalServerError, "Failed to get user active subscription")
+		// }
 
 		switch *body.Action {
 		case util.StreamActionPublish:
@@ -63,30 +63,30 @@ func postStreamEventsHandler(s *api.Server) echo.HandlerFunc {
 			BroadcastStreamStatus(s, stream.ID)
 
 		case util.StreamActionUnpublish:
-			// Get the stream's last published time to calculate duration
-			streamInfo, err := s.Queries.GetStreamById(ctx, stream.ID)
-			if err != nil {
-				log.Error().Err(err).Msg("Failed to get stream info")
-				return echo.NewHTTPError(http.StatusInternalServerError, "Failed to get stream info")
-			}
-
-			if streamInfo.LastPublishedAt.Valid {
-				// Calculate streaming duration in minutes
-				duration := time.Since(streamInfo.LastPublishedAt.Time)
-				streamingMinutes := int32(duration.Minutes())
-
-				// Update daily streaming usage
-				err = s.Queries.UpdateDailyStreamingUsage(ctx, database.UpdateDailyStreamingUsageParams{
-					UserID:               streamInfo.UserID,
-					SubscriptionID:       activeSub.ID,
-					StreamingMinutesUsed: streamingMinutes,
-					UsageDate:            time.Now().UTC().Truncate(24 * time.Hour),
-				})
-				if err != nil {
-					log.Error().Err(err).Msg("Failed to update streaming usage")
-				}
-			}
-
+			// // Get the stream's last published time to calculate duration
+			// streamInfo, err := s.Queries.GetStreamById(ctx, stream.ID)
+			// if err != nil {
+			// 	log.Error().Err(err).Msg("Failed to get stream info")
+			// 	return echo.NewHTTPError(http.StatusInternalServerError, "Failed to get stream info")
+			// }
+			//
+			// if streamInfo.LastPublishedAt.Valid {
+			// 	// Calculate streaming duration in minutes
+			// 	duration := time.Since(streamInfo.LastPublishedAt.Time)
+			// 	streamingMinutes := int32(duration.Minutes())
+			//
+			// 	// Update daily streaming usage
+			// 	err = s.Queries.UpdateDailyStreamingUsage(ctx, database.UpdateDailyStreamingUsageParams{
+			// 		UserID:               streamInfo.UserID,
+			// 		SubscriptionID:       activeSub.ID,
+			// 		StreamingMinutesUsed: streamingMinutes,
+			// 		UsageDate:            time.Now().UTC().Truncate(24 * time.Hour),
+			// 	})
+			// 	if err != nil {
+			// 		log.Error().Err(err).Msg("Failed to update streaming usage")
+			// 	}
+			// }
+			//
 			err = s.Queries.UnpublishStream(ctx, stream.ID)
 			if err != nil {
 				log.Error().Err(err).Msg("Failed to unpublish stream")
